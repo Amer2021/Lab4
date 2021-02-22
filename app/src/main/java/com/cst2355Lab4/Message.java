46487961e3d05bd6fac6ba7b7b596ab6db28035a
Amer Chireh <amerchireh02@gmail.com>
2021-02-21 19:23:07 -0500

    Lab 4

---
+++ app/src/main/java/com/cst2355Lab4/Message.java
@@ -0,0 +1,11 @@
+package com.cst2355Lab4;
+
+public class Message {
+    public boolean left;
+    public String message;
+
+    public Message(boolean left, String message) {
+        this.left = left;
+        this.message = message;
+    }
+}
